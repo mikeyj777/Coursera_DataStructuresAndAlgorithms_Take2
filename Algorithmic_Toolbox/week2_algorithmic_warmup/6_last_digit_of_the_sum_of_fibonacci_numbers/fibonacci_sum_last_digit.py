a91b9f204b9e1cc2afc747cc1a2b904877897c9e
Michael James <mikeyj777@gmail.com>
2021-02-18 23:15:41 -0500

    fib_s

--- Algorithmic_Toolbox/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
+++ Algorithmic_Toolbox/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
@@ -20,19 +20,18 @@
 #     n = int(input)
 #     print(fibonacci_sum_naive(n))
 
-import copy
-
-fib_dict = {}
-fib_dict[0] = 0
-fib_dict[1] = 1
-
 def fib_sum_last_dig(n):
-    global fib_dict
+    fib_dict = {}
+    fib_dict[-1] = 0
+    fib_dict[-2] = 0
+    fib_dict[0] = 0
+    fib_dict[1] = 1
 
     ans = 1
 
-    for i in range(2,n+1):
-        fib_dict[i] = (fib_dict[i-1] + fib_dict[i-2]) % 10
+    for i in range(0,n+1):
+        if i != 1:
+            fib_dict[i] = (fib_dict[i-1] + fib_dict[i-2]) % 10
         ans += fib_dict[i]
         ans %= 10
     
@@ -41,16 +40,4 @@ def fib_sum_last_dig(n):
 
 if __name__ == '__main__':
     input_n = int(input())
-    # n, m = [int(x) for x in input().split()]
-    # if input_n < 2:
-    #     print(input_n)
-    # else:
-    #     for i in range(2,input_n+1):
-    #         print(fib_sum_last_dig(i))
-    if input_n <= 2:
-        if input_n < 2:
-            print(input_n)
-        else:
-            print(2)
-    else:
-        print(fib_sum_last_dig(input_n))
+    print(fib_sum_last_dig(input_n))
--- Algorithmic_Toolbox/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
+++ Algorithmic_Toolbox/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
@@ -24,17 +24,19 @@
 
 import copy
 
-fib_dict = {}
-fib_dict[0] = 0
-fib_dict[1] = 1
-
 def fib_partial_sum_last_dig(m, n):
-    global fib_dict
-
+    
+    fib_dict = {}
+    fib_dict[-1] = 0
+    fib_dict[-2] = 0
+    fib_dict[0] = 0
+    fib_dict[1] = 1
+    
     ans = 0
 
-    for i in range(2,n+1):
-        fib_dict[i] = (fib_dict[i-1] + fib_dict[i-2]) % 10
+    for i in range(0,n+1):
+        if i != 1:
+            fib_dict[i] = (fib_dict[i-1] + fib_dict[i-2]) % 10
         if i >= m:
             ans += fib_dict[i]
             ans %= 10
@@ -45,10 +47,4 @@ def fib_partial_sum_last_dig(m, n):
 if __name__ == '__main__':
     # input_n = int(input())
     f_low, f_high = [int(x) for x in input().split()]
-    if f_high <= 2:
-        if f_high < 2:
-            print(f_high)
-        else:
-            print(2)
-    else:
-        print(fib_partial_sum_last_dig(f_low, f_high))
+    print(fib_partial_sum_last_dig(f_low, f_high))
