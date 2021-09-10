8d1dde8ea40d43d44f7f4a633f70ab99ea115150
Michael James <mikeyj777@gmail.com>
2021-09-10 00:15:11 -0400

    3_7_looks_good

---
+++ Algorithmic_Toolbox/week3_greedy_algorithms/7_maximum_salary/largest_number.py
@@ -0,0 +1,30 @@
+#Uses python3
+
+import sys
+
+def largest_number(a):
+    #write your code here
+    res = []
+    for x in a:
+        list_x = list(str(x))
+        for i in list_x:
+            res.append(i)
+    
+    res.sort(reverse=True)
+
+    ans = 0
+
+    currpow = 0
+
+    for i in range(len(res)-1,-1,-1):
+        ans += int(res[i])*10**currpow
+        currpow += 1
+
+    return ans
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    data = input.split()
+    a = data[1:]
+    print(largest_number(a))
+    
