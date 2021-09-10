a0cb5b4e9250d1b78d10b66d3c85433c521251cb
Michael James <mikeyj777@gmail.com>
2021-09-10 00:22:33 -0400

    3_7_looking better

--- Algorithmic_Toolbox/week3_greedy_algorithms/7_maximum_salary/largest_number.py
+++ Algorithmic_Toolbox/week3_greedy_algorithms/7_maximum_salary/largest_number.py
@@ -4,23 +4,32 @@ import sys
 
 def largest_number(a):
     #write your code here
-    res = []
-    for x in a:
-        list_x = list(str(x))
-        for i in list_x:
-            res.append(i)
+    # res = []
+    # for x in a:
+    #     list_x = list(str(x))
+    #     for i in list_x:
+    #         res.append(i)
     
-    res.sort(reverse=True)
+    # res.sort(reverse=True)
 
-    ans = 0
+    # ans = 0
 
-    currpow = 0
+    # currpow = 0
 
-    for i in range(len(res)-1,-1,-1):
-        ans += int(res[i])*10**currpow
-        currpow += 1
+    # for i in range(len(res)-1,-1,-1):
+    #     ans += int(res[i])*10**currpow
+    #     currpow += 1
+    
+    # return ans
 
-    return ans
+
+    res = ""
+
+    a.sort(reverse=True)
+
+    for x in a:
+        res += x
+    return res
 
 if __name__ == '__main__':
     input = sys.stdin.read()
