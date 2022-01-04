f97a09049c56a4861ba5cd9dd61f1ca534720cbd
Michael James <mikeyj777@gmail.com>
2022-01-03 22:36:12 -0500

    4_2_first try

--- Algorithmic_Toolbox/week4_divide_and_conquer copy/2_majority_element/majority_element.py
+++ Algorithmic_Toolbox/week4_divide_and_conquer copy/2_majority_element/majority_element.py
@@ -6,12 +6,28 @@ def get_majority_element(a, left, right):
         return -1
     if left + 1 == right:
         return a[left]
-    #write your code here
+    
+    elems = {}
+
+    n = (right - left) + 1
+    if left == 0:
+        n -= 1
+
+    for i in range(len(a)):
+        if a[i] in elems.keys():
+            elems[a[i]] += 1
+            if elems[a[i]] >= n / 2:
+                return 1
+        else:
+            elems[a[i]] = 1
+
     return -1
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *a = list(map(int, input.split()))
+    # input = sys.stdin.read()
+    # n, *a = list(map(int, input.split()))
+    a = [2, 3, 9, 2, 2]
+    n = len(a)
     if get_majority_element(a, 0, n) != -1:
         print(1)
     else:
