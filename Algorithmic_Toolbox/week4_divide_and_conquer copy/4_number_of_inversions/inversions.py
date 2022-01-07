c1c53aa673527223c3cddb488c7506bc2ad2bab3
Michael James <mikeyj777@gmail.com>
2022-01-07 00:09:03 -0500

    4_4_sounds good

--- Algorithmic_Toolbox/week4_divide_and_conquer copy/4_number_of_inversions/inversions.py
+++ Algorithmic_Toolbox/week4_divide_and_conquer copy/4_number_of_inversions/inversions.py
@@ -8,11 +8,15 @@ def get_number_of_inversions(a, b, left, right):
     ave = (left + right) // 2
     number_of_inversions += get_number_of_inversions(a, b, left, ave)
     number_of_inversions += get_number_of_inversions(a, b, ave, right)
-    #write your code here
+    for i in range(left,right):
+        if a[i] > a[i+1]:
+            number_of_inversions += 1
     return number_of_inversions
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *a = list(map(int, input.split()))
+    # input = sys.stdin.read()
+    # n, *a = list(map(int, input.split()))
+    a = [0, 1, 0]
+    n = len(a)
     b = n * [0]
-    print(get_number_of_inversions(a, b, 0, len(a)))
+    print(get_number_of_inversions(a, b, 0, n - 1))
