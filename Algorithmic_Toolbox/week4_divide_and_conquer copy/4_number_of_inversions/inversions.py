ea51fb59c8191d85b90b0f248e8bbcd6108ff999
Michael James <mikeyj777@gmail.com>
2022-01-09 00:16:20 -0500

    4_4_inversions_getting there

--- Algorithmic_Toolbox/week4_divide_and_conquer copy/4_number_of_inversions/inversions.py
+++ Algorithmic_Toolbox/week4_divide_and_conquer copy/4_number_of_inversions/inversions.py
@@ -1,40 +1,40 @@
 # Uses python3
 import sys
 
-def my_sort(a):
+def my_sort(a, left, right):
     num_inversions = 0
     if len(a) < 2:
         return a, 0
-    i = -1
-    while i <len(a)-1:
+    i = left - 1
+    while i < right:
         i += 1
-        for j in range(i, len(a)):
+        for j in range(i + 1, right + 1):
             if i != j:
                 if a[i] > a[j]:
                     a[i], a[j] = a[j], a[i]
-                    print(a)
+                    # print(a)
                     num_inversions += 1
-                    i = -1
+                    i = left -1
                     break
 
     return a, num_inversions
 
 def merge_sort(a, left, right):
     num_inversions = 0
-    if right - left < 2:
+    if right - left < 1:
         return a, 0
-    if right - left == 2:
-        return my_sort(a)
+    if right - left == 1:
+        return my_sort(a, left, right)
     
     ave = (left + right) // 2
 
-    a, left_invs = merge_sort(a, left, ave)
+    a, left_invs = merge_sort(a, left, ave - 1)
     
     a, right_invs = merge_sort(a, ave, right)
 
     num_inversions += left_invs + right_invs
 
-    a, more_invs = my_sort(a)
+    a, more_invs = my_sort(a, left, right)
 
     return a, num_inversions + more_invs
 
@@ -45,8 +45,8 @@ if __name__ == '__main__':
     
     # print(a)
 
-    a, num_inversions = merge_sort(a, 0, len(a))
+    a, num_inversions = merge_sort(a, 0, len(a) - 1)
 
-    print(f'there were {num_inversions} inversion(s).  The sorted array is {a}.')
+    # print(f'there were {num_inversions} inversion(s).  The sorted array is {a}.')
 
     print(num_inversions)
