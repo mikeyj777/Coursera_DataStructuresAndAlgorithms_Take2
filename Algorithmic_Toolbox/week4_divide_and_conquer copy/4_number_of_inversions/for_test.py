f9123ddf74ddf723e30c172f9783cc7d2c28cabd
Michael James <mikeyj777@gmail.com>
2022-01-08 23:48:50 -0500

    4_3_ftw

---
+++ Algorithmic_Toolbox/week4_divide_and_conquer copy/4_number_of_inversions/for_test.py
@@ -0,0 +1,21 @@
+import sys
+# counter = 0
+
+# a = [2, 4, 3, 6, 1]
+# print(a)
+# i = -1
+
+# while i < len(a) - 1:
+#     i += 1
+#     for j in range(i, len(a)):
+#         if i != j:
+#             if a[i] > a[j]:
+#                 a[i], a[j] = a[j], a[i]
+#                 print(a)
+#                 i = -1
+#                 break
+
+age = 44
+name = "Miguel"
+
+f'Hola {name}, you are {age} years old.'
--- Algorithmic_Toolbox/week4_divide_and_conquer copy/4_number_of_inversions/inversions.py
+++ Algorithmic_Toolbox/week4_divide_and_conquer copy/4_number_of_inversions/inversions.py
@@ -1,22 +1,52 @@
 # Uses python3
 import sys
 
-def get_number_of_inversions(a, b, left, right):
-    number_of_inversions = 0
-    if right - left <= 1:
-        return number_of_inversions
+def my_sort(a):
+    num_inversions = 0
+    if len(a) < 2:
+        return a, 0
+    i = -1
+    while i <len(a)-1:
+        i += 1
+        for j in range(i, len(a)):
+            if i != j:
+                if a[i] > a[j]:
+                    a[i], a[j] = a[j], a[i]
+                    print(a)
+                    num_inversions += 1
+                    i = -1
+                    break
+
+    return a, num_inversions
+
+def merge_sort(a, left, right):
+    num_inversions = 0
+    if right - left < 2:
+        return a, 0
+    if right - left == 2:
+        return my_sort(a)
+    
     ave = (left + right) // 2
-    number_of_inversions += get_number_of_inversions(a, b, left, ave)
-    number_of_inversions += get_number_of_inversions(a, b, ave, right)
-    for i in range(left,right):
-        if a[i] > a[i+1]:
-            number_of_inversions += 1
-    return number_of_inversions
+
+    a, left_invs = merge_sort(a, left, ave)
+    
+    a, right_invs = merge_sort(a, ave, right)
+
+    num_inversions += left_invs + right_invs
+
+    a, more_invs = my_sort(a)
+
+    return a, num_inversions + more_invs
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    # a = [0, 1, 0]
-    # n = len(a)
-    b = n * [0]
-    print(get_number_of_inversions(a, b, 0, n - 1))
+    # a = [2, 4, 3, 6, 1]
+    
+    # print(a)
+
+    a, num_inversions = merge_sort(a, 0, len(a))
+
+    print(f'there were {num_inversions} inversion(s).  The sorted array is {a}.')
+
+    print(num_inversions)
