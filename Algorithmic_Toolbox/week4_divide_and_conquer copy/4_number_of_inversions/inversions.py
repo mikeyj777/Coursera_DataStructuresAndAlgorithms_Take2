782ef70a312f306b266f530e664d053704dcaeec
Michael James <mikeyj777@gmail.com>
2022-01-09 00:28:11 -0500

    4_4_stolen from geeks for geeks

--- Algorithmic_Toolbox/week4_divide_and_conquer copy/4_number_of_inversions/inversions.py
+++ Algorithmic_Toolbox/week4_divide_and_conquer copy/4_number_of_inversions/inversions.py
@@ -1,6 +1,8 @@
 # Uses python3
 import sys
 
+num_inversions = 0
+
 def my_sort(a, left, right):
     num_inversions = 0
     if len(a) < 2:
@@ -19,24 +21,68 @@ def my_sort(a, left, right):
 
     return a, num_inversions
 
-def merge_sort(a, left, right):
-    num_inversions = 0
-    if right - left < 1:
-        return a, 0
-    if right - left == 1:
-        return my_sort(a, left, right)
+# def merge_sort(a, left, right):
+#     num_inversions = 0
+#     if right - left < 1:
+#         return a, 0
+#     if right - left == 1:
+#         return my_sort(a, left, right)
     
-    ave = (left + right) // 2
+#     ave = (left + right) // 2
 
-    a, left_invs = merge_sort(a, left, ave - 1)
+#     a, left_invs = merge_sort(a, left, ave - 1)
     
-    a, right_invs = merge_sort(a, ave, right)
+#     a, right_invs = merge_sort(a, ave, right)
 
-    num_inversions += left_invs + right_invs
+#     num_inversions += left_invs + right_invs
 
-    a, more_invs = my_sort(a, left, right)
+#     a, more_invs = my_sort(a, left, right)
 
-    return a, num_inversions + more_invs
+#     return a, num_inversions + more_invs
+
+def merge_sort(arr):
+    # code from https://www.geeksforgeeks.org/merge-sort/
+    global num_inversions
+    if len(arr) > 1:
+
+         # Finding the mid of the array
+        mid = len(arr)//2
+
+        # Dividing the array elements
+        L = arr[:mid]
+
+        # into 2 halves
+        R = arr[mid:]
+
+        # Sorting the first half
+        merge_sort(L)
+
+        # Sorting the second half
+        merge_sort(R)
+
+        i = j = k = 0
+
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+                num_inversions += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -45,7 +91,7 @@ if __name__ == '__main__':
     
     # print(a)
 
-    a, num_inversions = merge_sort(a, 0, len(a) - 1)
+    merge_sort(a, 0, len(a) - 1)
 
     # print(f'there were {num_inversions} inversion(s).  The sorted array is {a}.')
 
