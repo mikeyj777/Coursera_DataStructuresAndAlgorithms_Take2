7abbb6164cc031e40625317a778b10d4336b1208
Michael James <mikeyj777@gmail.com>
2022-02-10 22:36:27 -0500

    5_1

--- Algorithmic_Toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ Algorithmic_Toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -1,9 +1,18 @@
 # Uses python3
 import sys
+import numpy as np
 
-def get_change(m):
-    #write your code here
-    return m // 4
+def get_change(money):
+    coins = [1, 3, 4]
+    min_num_coins = np.zeros((money+1))
+    for m in range(1,len(min_num_coins)):
+        min_num_coins[m] = np.inf
+        for coin in coins:
+            if m >= coin:
+                num_coins = min_num_coins[m - coin] + 1
+                min_num_coins[m] = min(min_num_coins[m], num_coins)
+    
+    return int(min_num_coins[money])
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
--- Algorithmic_Toolbox/week5_dynamic_programming1/1_money_change_again/mc_test1.py
+++ Algorithmic_Toolbox/week5_dynamic_programming1/1_money_change_again/mc_test1.py
@@ -9,8 +9,8 @@ def dpChange(money, coins):
                 num_coins = min_num_coins[m - coin] + 1
                 min_num_coins[m] = min(min_num_coins[m], num_coins)
     
-    return min_num_coins[money]
+    return min_num_coins[money] // 1
 
-a = dpChange(997, [2, 4, 8])
+a = dpChange(34, [1, 3, 4])
 
 print(a)
