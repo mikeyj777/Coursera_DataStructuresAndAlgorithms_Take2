7ec81a00cf7041dea94df306157e8786ae671beb
Michael James <mikeyj777@gmail.com>
2022-02-11 23:40:21 -0500

    5-2 - giving answers?

--- Algorithmic_Toolbox/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
+++ Algorithmic_Toolbox/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
@@ -4,20 +4,30 @@ import copy
 import numpy as np
 
 winners = []
+solns = {}
 def calculator(target, n=1, calc_chain = []):
-    global winners
+    global winners, solns
     calc_chain.append(n)
+   
     if n == target:
         winners.append(calc_chain)
+        for i in calc_chain:
+            if i in solns.keys():
+                if len(calc_chain[i:]) < len(solns[i]):
+                    solns[i] = [i] + calc_chain[i:]
+            else:
+                solns[i] = [i] + calc_chain[i:]
     if n < target:
         inputs = [n + 1, n * 2, n * 3]
         if min(inputs) <= target:
             for inp in inputs:
                 if inp <= target:
-                    
-                    if calc_chain == [1, 2, 2]:
-                        apple = 1
-                    calculator(target, inp, copy.deepcopy(calc_chain))
+                    if inp in solns.keys():
+                        if solns[inp][-1] == target:
+                            calc_chain.extend(solns[inp])
+                            winners.append(calc_chain)
+                    else:
+                        calculator(target, inp, copy.deepcopy(calc_chain))
 
 def calc_handler(n):
     calculator(n)
@@ -35,7 +45,7 @@ def calc_handler(n):
 # input = sys.stdin.read()
 # n = int(input)
 
-n = 0
+n = 5
 
 output = calc_handler(n)
 print(len(output) - 1)
