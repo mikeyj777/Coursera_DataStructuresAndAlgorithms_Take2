f1d484f9758400c8ab1233526eb820db69d1816e
Michael James <mikeyj777@gmail.com>
2022-02-24 21:03:11 -0500

    treeee

--- Coursera_Data_Structures_Course_Content/week1_basic_data_structures/class_examples/tree.py
+++ Coursera_Data_Structures_Course_Content/week1_basic_data_structures/class_examples/tree.py
@@ -10,6 +10,9 @@ class Tree:
 
 class Binary_Tree(Tree):
 
+    left = None
+    right = None
+
     def add_left(self, key):
         self.left = Binary_Tree()
         self.left.key = key
@@ -19,6 +22,29 @@ class Binary_Tree(Tree):
         self.right.key = key
     
     def height(self):
-        if self.left == None and self.right == None:
-            return 0
-        return 1 + max(self.left.height,self.right.height)
+        if self.left == None:
+            if self.right == None:
+                return 0
+            return 1 + self.right.height()
+
+        if self.right == None:
+            if self.left == None:
+                return 0
+            return 1 + self.left.height()
+
+        return 1 + max(self.left.height(), self.right.height())
+
+
+b_tree = Binary_Tree()
+
+b_tree.add_left('a')
+b_tree.add_right('b')
+b_tree.left.add_left('aa')
+b_tree.right.add_left('ba')
+b_tree.right.add_right('ba')
+
+b_tree.right.right.add_left('baa')
+
+print(b_tree.height())
+
+a = 1
